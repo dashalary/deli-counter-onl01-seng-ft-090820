db4e6f658b3716efff4db0289e7db82546da87b5
Dasha Lary <dk2800@nyu.edu>
2020-09-10 18:13:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,25 @@
 katz_deli = []
 
-def take_a_number(katz_deli, name)
-katz_deli.push(name)
-puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
+# def take_a_number(katz_deli, name)
+# katz_deli.push(name)
+# puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+# end
  
-# function nowServing(katzDeliLine) {
+# # function nowServing(katzDeliLine) {
    
-#   if (katzDeliLine.length === 0) {
-#   return `There is nobody waiting to be served!`
-# } else { return `Currently serving ${katzDeliLine.shift()}.`
-#   } 
-# }
+# #   if (katzDeliLine.length === 0) {
+# #   return `There is nobody waiting to be served!`
+# # } else { return `Currently serving ${katzDeliLine.shift()}.`
+# #   } 
+# # }
  
-def line
-if katz_deli.length == 0
-puts "The line is currently empty."
-else
-for (let i = 0; i < katz_deli.length; i++) 
-     line.push(`${i + 1}. ${katz_deli[i]}`)
+# def line
+# if katz_deli.length == 0
+# puts "The line is currently empty."
+# else
+# for (let i = 0; i < katz_deli.length; i++) 
+#     line.push(`${i + 1}. ${katz_deli[i]}`)
      
-puts `The line is currently: #{line.join(", ")}`
-end 
-end
+# puts `The line is currently: #{line.join(", ")}`
+# end 
+# end
