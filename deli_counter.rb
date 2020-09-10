4b64cfe934892290cfdfc7818569ad32c99b7226
Dasha Lary <dk2800@nyu.edu>
2020-09-10 18:01:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,19 +5,18 @@ katz_deli.push(name)
 puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
  
- function nowServing(katzDeliLine) {
+# function nowServing(katzDeliLine) {
    
-   if (katzDeliLine.length === 0) {
-   return `There is nobody waiting to be served!`
-} else { return `Currently serving ${katzDeliLine.shift()}.`
-   } 
- }
+#   if (katzDeliLine.length === 0) {
+#   return `There is nobody waiting to be served!`
+# } else { return `Currently serving ${katzDeliLine.shift()}.`
+#   } 
+# }
  
- function currentLine(katzDeliLine) {
-   const peopleInLine = [];
-   if (katzDeliLine.length === 0) {
-     return `The line is currently empty.`
-   } else { 
+def line
+if katz_deli.length == 0
+     puts "The line is currently empty."
+else
   
    for (let i = 0; i < katzDeliLine.length; i++) {
      peopleInLine.push(`${i + 1}. ${katzDeliLine[i]}`);
