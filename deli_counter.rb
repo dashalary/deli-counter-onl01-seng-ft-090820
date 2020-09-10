6e8f6b993fd4962e2cc725f32e768db291ed9cd2
Dasha Lary <dk2800@nyu.edu>
2020-09-10 18:58:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ elsif katz_deli.length > 0
 counter = 0 
 loop do 
 counter += 1
-katz_deli.each { |customer| puts "There are currently #{counter}. #{customer} in line" }
+katz_deli.each { |customer| puts "The line is currently: #{counter}. #{customer}" }
 
 
 end
